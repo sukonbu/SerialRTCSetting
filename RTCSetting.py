aa6d40b1f7250ed7e6f0667638759be26b5fd6ee
sukonbu <t.cfhhy.skn0909@gmail.com>
2017-06-13 22:20:31 +0900

    added test function

--- RTCSetting.py
+++ RTCSetting.py
@@ -5,14 +5,25 @@ import time
 #  serial test
 ser = serial.Serial()
 ser.baudrate = 9600
+print('portCheck')
 for i in range(100):
     try:
+        print('port found No.' + i)
         ser.port = i
         ser.open()
+        read_serial()
         break
     except:
         i = i
-while(true){
-    ser.write("Hello")
-    time.sleep(1.0)
-}
+print('end setting')
+
+
+def read_serial():
+    while True:
+        if ser.inWaiting() > 0:
+            data = ser.readline()
+            if 'uv' in data:
+                print('connected device')
+                ser.write('s')
+                break
+    # serial message to Device (RTCmodule)
